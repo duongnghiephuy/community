430f647bfa339bf5773384befae8c59f288c1a62
Huy <huydragon23@gmail.com>
2022-04-30 17:20:02 +0900

    add test for polls app

--- polls/tests.py
+++ polls/tests.py
@@ -1,3 +1,88 @@
+from email import message
+from urllib import response
 from django.test import TestCase
+from django.contrib.auth.models import User
+from django.utils import timezone
+from accounts.models import Community
+from polls.models import Question, Choice
+from django.contrib.gis.geos import Point
+from django.urls import reverse
+from django.test import Client
 
 # Create your tests here.
+
+
+class QuestionModelTests(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        # user is in the group we want to test
+        cls.user = User.objects.create_user(username="testuser", password="12345")
+        # @user1 is not in the group we want to test
+        cls.user1 = User.objects.create(username="testuser1", password="12345")
+
+        cls.community = Community.objects.create(
+            name="Test_Community",
+            description="This is a community for test",
+        )
+
+    def setUp(self):
+
+        # Create a live question
+        question_text = "Test Question"
+        pub_date = timezone.now()
+        self.question = Question.objects.create(
+            question_text=question_text,
+            pub_date=pub_date,
+            closed=False,
+            author=self.user,
+            community=self.community,
+        )
+        for choice_text in ["Choice 1", "Choice 2"]:
+            Choice.objects.create(question=self.question, choice_text=choice_text)
+
+        self.client = Client()
+
+    def test_is_voter(self):
+        self.assertIs(self.question.is_voter(self.user), True)
+        self.assertIs(self.question.is_voter(self.user1), False)
+
+    # test failed vote without data
+    def test_no_data_votemodal(self):
+
+        # login required for all votemodal test
+        self.client.login(username="testuser", password="12345")
+
+        response = self.client.post(
+            reverse("polls:votemodal", kwargs={"question_id": self.question.id})
+        )
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(
+            response.context["error_message"], "You didn't select a choice"
+        )
+
+    # test vote with data
+    def test_data_votemodal(self):
+        self.client.login(username="testuser", password="12345")
+        response = self.client.post(
+            reverse(
+                "polls:votemodal",
+                kwargs={"question_id": self.question.id},
+            ),
+            data={"choice": 1},
+        )
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.context["error_message"], None)
+
+        # test vote after changing choice
+        response = self.client.post(
+            reverse(
+                "polls:votemodal",
+                kwargs={"question_id": self.question.id},
+            ),
+            data={"choice": 2},
+        )
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.context["error_message"], "You changed your vote")
