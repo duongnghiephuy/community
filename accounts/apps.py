a960074491ece5644d04a10638abf14200e231e6
Huy <huydragon23@gmail.com>
2022-05-03 20:15:08 +0900

    add signal function to delete images of user,community after delete

--- accounts/apps.py
+++ accounts/apps.py
@@ -2,5 +2,9 @@ from django.apps import AppConfig
 
 
 class AccountsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'accounts'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "accounts"
+
+    def ready(self):
+        # Implicitly connect a signal handlers decorated with @receiver.
+        from . import signals
--- posts/signals.py
+++ posts/signals.py
@@ -1,7 +1,7 @@
 from django.core.files.storage import default_storage
 from django.dispatch import receiver
 from django.db.models.signals import post_delete
-from .models import Post
+from .models import Post, Community
 
 # This is to delete post_image in media folder after a post is deleted
 
@@ -10,6 +10,21 @@ from .models import Post
 def delete_associated_files(sender, instance, **kwargs):
 
     """Remove all files of an image after deletion."""
-    path = instance.post_image.name
-    if path:
-        default_storage.delete(path)
+    try:
+        path = instance.post_image.name
+        if path:
+            default_storage.delete(path)
+    except:
+        print("Check signal function")
+
+
+@receiver(post_delete, sender=Community)
+def delete_Community_associated_files(sender, instance, **kwargs):
+
+    """Remove all files of an image after deletion."""
+    try:
+        path = instance.image.name
+        if path:
+            default_storage.delete(path)
+    except:
+        print("Check signal function")
