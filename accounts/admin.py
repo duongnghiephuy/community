2b6ec008af1d62381a70e3bfa88da4b39d0273de
Huy <huydragon23@gmail.com>
2022-05-04 17:19:42 +0900

    add userprofile to admin panel

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Community, MemberRole
+from .models import Community, MemberRole, UserProfile
 
 admin.site.register(Community)
 admin.site.register(MemberRole)
+admin.site.register(UserProfile)
 # Register your models here.
